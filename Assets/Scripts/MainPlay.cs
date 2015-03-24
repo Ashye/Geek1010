67c5f525856a1be14d760e80977c1f2aa0f66573
ash <ashye@vip.qq.com>
2015-03-24 17:55:53 +0800

    init import

---
+++ Assets/Scripts/MainPlay.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainPlay : MonoBehaviour {
+
+	public GameObject			blockPrefab;
+
+
+	//public GameObject
+	public GameObject[,]	blocks;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	private void initGrid() {
+		blocks = new GameObject[10, 10];
+
+		for (int i=0; i<10; i++) {
+			for(int j=0; j<10; j++) {
+				blocks[i,j] = Instantiate();
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/ScoreBoard.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+
+public class ScoreBoard : MonoBehaviour {
+	
+	public bool					_______________;
+
+	static public int 			score = 0;
+	static public int 			highest = 100;
+	private Text				tScore;
+	private Text				tHighest;
+
+	void Awake() {
+
+		if (PlayerPrefs.HasKey ("score_highest")) {
+			highest = PlayerPrefs.GetInt("score_highest");
+		}
+
+		Text[] gos = this.GetComponentsInChildren<Text> ();
+		if (gos.Length == 2) {
+			if (gos[0].name == "Score") {
+				tScore = gos[0];
+				tHighest = gos[1];
+			}else {
+				tScore = gos[1];
+				tHighest = gos[0];
+			}
+		}
+
+		PlayerPrefs.SetInt ("score_highest", highest);
+	}
+
+	// Use this for initialization
+	void Start () {
+		if (tHighest != null) {
+			tHighest.text = ""+highest;
+		}
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (score > highest) {
+			highest = score;
+			PlayerPrefs.SetInt ("score_highest", highest);
+			tHighest.text =  "" +highest;
+		}
+
+		tScore.text = "" +score;
+	}
+
+	void OnDestroy() {
+		PlayerPrefs.DeleteAll();
+	}
+}
