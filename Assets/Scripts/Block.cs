7fa5eb6b2d6290af7f845dee6b0131e11e84b201
ash <ashye@vip.qq.com>
2015-03-26 11:30:25 +0800

    update

---
+++ Assets/Scripts/Block.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Block : MonoBehaviour {
+
+
+	static Block		B;
+	private bool			_empty = true;
+
+
+
+	void Awake() {
+		B = this;
+	}
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public float CoverRate(Bounds cover) {
+		print ("=================");
+		print (transform.position);
+		print (renderer.bounds.center);
+		print (renderer.bounds.size);
+		print ("----------");
+		print (cover.center);
+		print (cover.size);
+		Vector3 point1 = Vector3.zero;
+		Vector3 point2 = Vector3.zero;
+
+		point1.x = transform.position.x - renderer.bounds.size.x / 2;
+		point1.y = cover.center.y + cover.size.y / 2;
+
+		point2.y = transform.position.y - renderer.bounds.size.y / 2;
+		point2.x = cover.center.x + cover.size.x / 2;
+
+		Vector3 point = point1 - point2;
+
+		float area = Mathf.Abs (point.x) * Mathf.Abs (point.y);
+		print ("cover area:"+area);
+
+		return area;
+	}
+
+	public bool empty {
+		get {
+			return _empty;
+		}
+		set {
+			_empty = value;
+		}
+	}
+
+//	void OnTriggerEnter(Collider other) {
+//		print ("trigger enter");
+//	}
+//	
+//	void OnTriggerStay(Collider other) {
+//		print ("trigger stay");
+//		
+//		//if 
+//	}
+//	
+//	void OnTriggerExit(Collider other) {
+//		print ("trigger exit");
+//	}
+}
---
+++ Assets/Scripts/Shape.cs
@@ -0,0 +1,118 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Shape : MonoBehaviour {
+	//public bool					__________________________;
+
+	private Vector3				oldScale;
+	private Vector3				oldPos;
+	
+	private List<GameObject>	lColliders;
+	private bool				blockMoving = false;
+
+	void Awake() {
+		oldScale = gameObject.transform.localScale;
+		lColliders = new List<GameObject> ();
+	}
+	// Use this for initialization
+	void Start () {
+		oldPos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	IEnumerator OnMouseDown() {
+		blockMoving = true;
+		gameObject.transform.localScale *= 1.1f;
+
+
+		Vector3 screenSpace = Camera.main.WorldToScreenPoint (transform.position);
+		Vector3 offset = transform.position - Camera.main.ScreenToWorldPoint (new Vector3 (
+			Input.mousePosition.x, Input.mousePosition.y, screenSpace.z));
+
+		while (Input.GetMouseButton(0)) {
+			Vector3 curScreenSpace = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenSpace.z);
+			Vector3 Curpos = Camera.main.ScreenToWorldPoint(curScreenSpace)+offset;
+			transform.position = Curpos;
+			yield return new WaitForFixedUpdate();
+		}
+	}
+
+	void OnMouseUp() {
+		blockMoving = false;
+		gameObject.transform.localScale = oldScale;
+
+
+		//检查当前待填充检查块
+		foreach(GameObject go in lColliders) {
+			Block script = go.GetComponent<Block>();
+			if (script.CoverRate(renderer.bounds) <=0.5f) {
+				lColliders.Remove(go);
+			}
+		}
+
+		print(lColliders.Count);
+		//检查能不能放下，能放下则销毁本对象，并填充网格，不能则图案返回原始位置
+
+		if (CheckSpace()) {
+			FillBlock();
+			lColliders.Clear();
+			Destroy(gameObject);
+		} else {
+			lColliders.Clear();
+			transform.position = oldPos;
+		}
+		print ("colliders:"+lColliders.Count);
+	}
+
+	private bool CheckSpace() {
+		if (lColliders.Count <= 0) {
+			return false;
+		} else {
+			Block blockScript = null;
+			foreach(GameObject go in lColliders) {
+				blockScript = go.GetComponent<Block>();
+				if (blockScript != null && blockScript.empty) {
+					continue;
+				}else {
+					return false;
+				}
+			}
+			return true;
+		}
+	}
+
+	private void FillBlock() {
+		if (lColliders.Count > 0) {
+			foreach(GameObject go in lColliders) {
+				go.renderer.material.color = gameObject.renderer.material.color;
+				go.GetComponent<Block>().empty = false;
+			}
+		}
+	}
+
+	void OnTriggerEnter(Collider other) {
+		//print ("shape trigger enter");
+		if (blockMoving) {
+			lColliders.Add (other.gameObject);
+		}
+	}
+
+//	void OnTriggerStay(Collider other) {
+//		print ("trigger stay");
+//
+//		//if 
+//	}
+
+	void OnTriggerExit(Collider other) {
+		//print ("shape trigger exit");
+		if (blockMoving) {
+			lColliders.Remove (other.gameObject);
+		}
+		//print ();
+	}
+}
